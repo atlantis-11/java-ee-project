e70e448404584d28f4e2f11d175bfa21b18e58a4
atlantis-11 <85559317+atlantis-11@users.noreply.github.com>
2024-04-28 22:10:53 +0300

    Added simple book page and added some styling

--- src/main/java/com/example/javaeeproject/controller/BookController.java
+++ src/main/java/com/example/javaeeproject/controller/BookController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -20,12 +21,21 @@ public class BookController {
         this.bookService = bookService;
     }
 
-    @GetMapping("/list")
+    @GetMapping("/")
     public String listBooks(Model model) {
         List<Book> books = bookService.findAll();
 
         model.addAttribute("books", books);
 
         return "list-books";
+    }
+
+    @GetMapping("/{id}")
+    public String showBook(@PathVariable int id, Model model) {
+        Book book = bookService.findById(id);
+
+        model.addAttribute("book", book);
+
+        return "book-page";
     }
 }
--- src/main/java/com/example/javaeeproject/entity/Book.java
+++ src/main/java/com/example/javaeeproject/entity/Book.java
@@ -21,6 +21,12 @@ public class Book {
     @Column(name = "author")
     private String author;
 
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "cover_url")
+    private String coverUrl;
+
     public Book() {
 
     }
